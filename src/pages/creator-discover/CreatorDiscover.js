603e114af91d248fc05b9a2c14fa422b925d0e66
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2022-07-24 01:25:11 +0530

    feat(ui): Add search in the discover page

--- src/pages/creator-discover/CreatorDiscover.js
+++ src/pages/creator-discover/CreatorDiscover.js
@@ -1,21 +1,36 @@
 import React, { Component } from "react";
 import { GlobalContext } from "../../context/context";
-import CreatorList from "./CreatorList";
+
 import Search from "./Search";
 
+import CreatorCard from "../CreatorCard";
+
 export default class CreatorDiscover extends Component {
-  static contextType = GlobalContext
+  static contextType = GlobalContext;
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
   render() {
     return (
-      <>
-      <Search />
-      <CreatorList />
-      </>
+      <section className="w-full py-12 bg-white lg:py-24">
+        <div className="max-w-6xl px-12 mx-auto text-center">
+          <div className="space-y-12 md:text-center">
+            <div className="max-w-3xl mb-20 space-y-5 sm:mx-auto sm:space-y-4">
+              <h2 className="relative text-4xl font-extrabold tracking-tight sm:text-5xl">Discover the Creators</h2>
+            </div>
+          </div>
+          <Search/>
+
+          <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
+            <CreatorCard /> <CreatorCard /> <CreatorCard />
+            <CreatorCard /> <CreatorCard /> <CreatorCard />
+            <CreatorCard /> <CreatorCard /> <CreatorCard />
+          </div>
+        </div>
+      </section>
     );
   }
 }
+
--- src/pages/creator-discover/Search.js
+++ src/pages/creator-discover/Search.js
@@ -2,20 +2,20 @@ import React from "react";
 
 export default function Example() {
   return (
-    <div className="p-5"> 
-       <label htmlFor="search" className="block text-sm font-medium text-gray-700">
-        Quick search
-      </label>
+    <div className="p-5">
       <div className="mt-1 relative flex items-center">
         <input
           type="text"
           name="search"
           id="search"
+          placeholder="Search your favorite creators"
           className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-12 sm:text-sm border-gray-300 rounded-md"
         />
         <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
-          <kbd className="inline-flex items-center border border-gray-200 rounded px-2 text-sm font-sans font-medium text-gray-400">
-            ⌘K
+          <kbd className="inline-flex items-center   rounded px-2 text-sm font-sans font-medium text-gray-400">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
           </kbd>
         </div>
       </div>
