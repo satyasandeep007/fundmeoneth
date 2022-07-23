173ed5c16f129e78b9deba9aff80d885edd1ab2a
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2022-07-24 03:19:49 +0530

    feat(ui): Add Dashboard UI

--- src/pages/dashboard/Analytics.js
+++ src/pages/dashboard/Analytics.js
@@ -1,5 +1,5 @@
-import React, { Fragment, useState, useEffect } from "react";
-import { Link ,Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -8,7 +8,7 @@ function classNames(...classes) {
 const Analytics = () => {
   const [tabs, setTabs] = useState([
     { name: "User", href: "/analytics/dashboard/user", count: "4", current: false },
-    { name: "Creator", href: "/analytics/dashboard/creator", count: "22", current: true },
+    { name: "Creator", href: "/analytics/dashboard/creator", count: "22", current: true }
   ]);
 
   const onTabSelect = (val) => {
@@ -31,31 +31,26 @@ const Analytics = () => {
       <div className="min-h-full">
         <main className="pt-8 pb-16">
           <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-           
-                <div className="border-b border-gray-200">
-                  <nav className="mt-2 -mb-px flex space-x-8" aria-label="Tabs">
-                    {tabs.map((tab, i) => (
-                      <Link
-                        key={tab.name}
-                        to={tab.href}
-                        onClick={() => onTabSelect(i)}
-                        className={classNames(
-                          tab.current
-                            ? "border-purple-500 text-purple-600"
-                            : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200",
-                          "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
-                        )}>
-                        {tab.name}
-                      </Link>
-                    ))}
-                  </nav>
-                 
-                </div>
-               
-             
-         
-            
-                <Outlet />
+            <div className="border-b border-gray-200">
+              <nav className="mt-2 -mb-px flex space-x-8" aria-label="Tabs">
+                {tabs.map((tab, i) => (
+                  <Link
+                    key={tab.name}
+                    to={tab.href}
+                    onClick={() => onTabSelect(i)}
+                    className={classNames(
+                      tab.current
+                        ? "border-purple-500 text-purple-600"
+                        : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200",
+                      "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
+                    )}>
+                    {tab.name}
+                  </Link>
+                ))}
+              </nav>
+            </div>
+
+            <Outlet />
           </div>
         </main>
       </div>
