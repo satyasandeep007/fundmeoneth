461642bd1f85b7b982d9ae72c755857828e52d8f
Sandeep Kumar <satyasandeep786@gmail.com>
2022-07-24 06:46:17 +0530

    feat: integrate smart contract data in analytics

--- src/pages/dashboard/UserDashboard.js
+++ src/pages/dashboard/UserDashboard.js
@@ -1,15 +1,30 @@
 /* This example requires Tailwind CSS v2.0+ */
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Transactions from "./Transactions";
+import { GlobalContext } from "../../context/context";
 
 /* This example requires Tailwind CSS v2.0+ */
-const stats = [
+const initialStats = [
   { name: "Total fund Donated", stat: "71,897 eth" },
-  { name: "Total Creators Funded", stat: "7" },
- 
+  { name: "Total Creators Funded", stat: "7" }
 ];
 
 export default function Example() {
+  const { accounts, creatorData } = useContext(GlobalContext);
+  const [stats, setStats] = useState(initialStats);
+
+  useEffect(() => {
+    const creatorD = creatorData.map((item) => {
+      if (item.walletAddress == accounts[0]) {
+        return item;
+      }
+    })[0];
+    const stat = [
+      { name: "Total fund Donated", stat: `${creatorD.totalFundsSent} ETH` },
+      { name: "Total Creators Funded", stat: creatorD.totalCreatorsFundedCount }
+    ];
+    setStats(stat);
+  }, []);
   return (
     <div>
       <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2">
