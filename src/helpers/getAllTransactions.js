838bdd78eb070e5c83a9f78d2ef81c96bbe61a41
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2022-07-24 07:03:36 +0530

    fix(ethers): Seperate from and to transactions

--- src/helpers/getAllTransactions.js
+++ src/helpers/getAllTransactions.js
@@ -2,18 +2,22 @@ export default (myaccount, provider) => {
   const endBlockNumber = provider.blockNumber();
   const startBlockNumber = endBlockNumber - 1000;
 
-  const transactions = [];
+  const toTransactions = [];
+  const fromTransactions = [];
 
   for (var i = startBlockNumber; i <= endBlockNumber; i++) {
     var block = provider.getBlockWithTransactions(i);
     if (block != null && block.transactions != null) {
       block.transactions.forEach(function (e) {
-        if (myaccount == "*" || myaccount == e.from || myaccount == e.to) {
-          transactions.push(e);
+        if (myaccount == e.to) {
+          toTransactions.push(e);
+        }
+        if (myaccount == e.from) {
+          fromTransactions.push(e);
         }
       });
     }
   }
 
-  return transactions;
+  return { toTransactions, fromTransactions };
 };
