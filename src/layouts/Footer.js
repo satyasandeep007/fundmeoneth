f61596363247ca0cdbfc1095c68a6ab19bea860f
Sandeep Kumar <satyasandeep786@gmail.com>
2022-07-24 11:09:30 +0530

    Add footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -7,7 +7,9 @@ export default function Footer() {
         <a href="#_" className="text-xl font-black leading-none text-gray-900 select-none logo">
           fundmeonETH<span className="text-indigo-600">.</span>
         </a>
-        <p className="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">© 2021 Tails - Tailwindcss Page Builder</p>
+        <p className="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">
+          © 2022 - Shiva Kumar Mangina , Sandeep Kumar
+        </p>
         <span className="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
           <a href="#" className="text-gray-400 hover:text-gray-500">
             <span className="sr-only">Facebook</span>
