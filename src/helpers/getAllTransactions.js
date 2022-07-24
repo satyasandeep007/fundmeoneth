9aa35c18cdfda6a2737b628251c25085277352bc
Sandeep Kumar <satyasandeep786@gmail.com>
2022-07-24 07:39:39 +0530

    fix: add validations and fix promises

--- src/helpers/getAllTransactions.js
+++ src/helpers/getAllTransactions.js
@@ -1,12 +1,10 @@
-export default (myaccount, provider) => {
-  const endBlockNumber = provider.blockNumber();
-  const startBlockNumber = endBlockNumber - 1000;
-
+export const getTransactions = async (myaccount, provider) => {
+  const endBlockNumber = await provider.getBlockNumber();
+  const startBlockNumber = 0;
   const toTransactions = [];
   const fromTransactions = [];
-
   for (var i = startBlockNumber; i <= endBlockNumber; i++) {
-    var block = provider.getBlockWithTransactions(i);
+    var block = await provider.getBlockWithTransactions(i);
     if (block != null && block.transactions != null) {
       block.transactions.forEach(function (e) {
         if (myaccount == e.to) {
